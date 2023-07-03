1a74d419ff0ae9b16f786c1a6e969adac86b640b
nplatte <77999574+nplatte@users.noreply.github.com>
2023-07-03 11:53:38 -0400

    updated first FT to have a set up and tear down

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,7 +1,36 @@
 from selenium import webdriver
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.contrib.auth.models import User
+from selenium.webdriver.common.by import By
 
 class TestCanPost(StaticLiveServerTestCase):
 
+    def setUp(self):
+        # creates user and goes to the home page
+        self.test_username = 'nplatte'
+        self.test_password = 'swordpass123567'
+        User.objects.create_user(self.test_username, 'test@test.com', self.test_password)
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.live_server_url)
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+
     def test_user_can_make_post(self):
-        pass
+        # I navigate to the website
+        # I see no posts up so I decide to make one
+        post_number = self.browser.find_elements(By.CLASS_NAME, 'post')
+        self.assertEqual(0, len(post_number))
+        # I see the nav bar with a Home, Find Post, About, and Sign In and click sign in
+        # this takes me to a log in screen where I enter in my credentials
+        # I enter them in and am taken to a new screen
+        # decide my first book will be V.E. Shwwab's "A Darker Shade of Magic"
+        # I enter in the author and book name
+        # I add the tags that are appropriate
+        # I add my thoughts on the first two chapters and hit post
+        # I click the home button and see my post on the recent posts
+        post_number = self.browser.find_elements(By.CLASS_NAME, 'post')
+        self.assertEqual(1, len(post_number))
+        # I click the new post and see what just got done posting
+        
