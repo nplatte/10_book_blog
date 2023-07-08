e759a6e647c9d74dff2087c6a0a81f4d13dabf1f
nplatte <77999574+nplatte@users.noreply.github.com>
2023-07-08 07:48:31 -0400

    added test for tag model representation

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,13 @@ from django.db import models
 class Post(models.Model):
     
     title = models.CharField(max_length=20)
+    author = models.CharField(max_length=20)
+    post = models.TextField()
 
     def __str__(self):
         return self.title
+    
+
+class Tag(models.Model):
+
+    pass
--- posts/tests/test_models.py
+++ posts/tests/test_models.py
@@ -1,4 +1,4 @@
-from posts.models import Post
+from posts.models import Post, Tag
 from django.test import TestCase
 
 
@@ -10,3 +10,10 @@ class TestPost(TestCase):
         post = Post.objects.create(title=test_title)
         self.assertEqual(str(post), test_title)
 
+
+class TestTag(TestCase):
+
+    def test_string_representation(self):
+        test_tag_name = "V.W. Schwab"
+        tag = Tag.objects.create(tag_name=test_tag_name)
+        self.assertEqual(str(tag), test_tag_name)
