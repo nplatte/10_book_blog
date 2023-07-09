dfab974e9926d8fd94a258666f0f274653459ebd
nplatte <77999574+nplatte@users.noreply.github.com>
2023-07-09 14:56:33 -0400

    Added the proper ids to the form fields

--- posts/forms.py
+++ posts/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from posts.models import Post
 
 class PostModelForm(ModelForm):
@@ -6,3 +7,9 @@ class PostModelForm(ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'book_author', 'book_title', 'post']
+        widgets = {
+            'book_author': forms.TextInput(attrs={'id': 'author_entry'}),
+            'title': forms.TextInput(attrs={'id': 'title_entry'}),
+            'post': forms.Textarea(attrs={'id': 'post_entry'}),
+            'book_title': forms.TextInput(attrs={'id': 'book_title_entry'})
+        }
--- posts/tests/test_forms.py
+++ posts/tests/test_forms.py
@@ -8,4 +8,13 @@ class TestPostModelForm(TestCase):
                                   'book_title': 'A Darker Side of Magic',
                                   'post': 'blahblahblah'})
         self.assertTrue(new_form.is_valid())
+
+    def test_form_has_right_identifying_information(self):
+        form = PostModelForm()
+        form_as_html = form.as_p()
+        self.assertIn('id="author_entry"', form_as_html)
+        self.assertIn('id="title_entry"', form_as_html)
+        self.assertIn('id="post_entry"', form_as_html)
+        self.assertIn('id="book_title_entry"', form_as_html)
+
         
