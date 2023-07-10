0a5a3a3fa55981dfa028359acf8af0398f5ea5ba
nplatte <77999574+nplatte@users.noreply.github.com>
2023-07-10 13:10:26 -0400

    added failing test to view for getting filtered tags passed to template

--- posts/tests/test_views.py
+++ posts/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from posts.models import Post
+from posts.models import Post, Tag
 from posts.forms import PostModelForm, TagForm
 from django.contrib.auth.models import User
 from posts.views import create_post_page
@@ -10,16 +10,31 @@ from posts.views import create_post_page
 # test that the forms are the right forms  to use
 # 
 class TestHomeView(TestCase):
+    def setUp(self):
+        self.response = self.client.get(reverse('home_page'))
 
     def test_view_returns_right_template(self):
-        self.client.get(reverse('home_page'))
         self.assertTemplateUsed('posts/home.html')
 
     def test_context_contains_post_requests(self):
         Post.objects.create(title='test')
         response = self.client.get(reverse('home_page'))
-        
         self.assertEqual(len(response.context['posts']), 1)
+
+    def test_view_tag_context(self):
+        t1 = Tag.objects.create(tag_name='general')
+        t2 = Tag.objects.create(tag_name='book')
+        t3 = Tag.objects.create(tag_name='author')
+        self.assertEqual(len(Tag.objects.all()), 3)
+        general_tags = self.response.context['general_tags']
+        self.assertEqual(len(general_tags), 1)
+        self.assertEqual(general_tags, t1)
+        author_tags = self.response.context['author_tags']
+        self.assertEqual(len(author_tags), 1)
+        self.assertEqual(author_tags[0], t2)
+        book_series_tags = self.response.context['book_tags']
+        self.assertEqual(len(book_series_tags), 1)
+        self.assertEqual(book_series_tags[0], t3)
 
 
 class TestGETCreatePostView(TestCase):
@@ -32,11 +47,13 @@ class TestGETCreatePostView(TestCase):
     def test_view_returns_right_template(self):
         self.assertTemplateUsed('posts/create.html')
 
-    def test_view_context(self):
+    def test_view_form_context(self):
         form = self.response.context['new_post_form']
         self.assertIsInstance(form, PostModelForm)
         form = self.response.context['post_tags_form']
         self.assertIsInstance(form, TagForm)
+
+
 
 
 class TestPOSTCreatePostView(TestCase):
--- posts/views.py
+++ posts/views.py
@@ -2,23 +2,29 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from posts.forms import PostModelForm, TagForm
-from posts.models import Post
+from posts.models import Post, Tag
 
 def home_page(request):
     posts = Post.objects.all()
-    return render(request, 'posts/home.html', {'posts': posts})
+    context = {
+        'posts': posts,
+        'general_tags': Tag.objects.all()}
+    return render(request, 'posts/home.html', context)
 
 @login_required
 def create_post_page(request):
     new_post_form = PostModelForm()
     post_tags_form = TagForm()
+    context = {
+        'new_post_form': new_post_form, 
+        'post_tags_form': post_tags_form}
     if request.method == "POST":
         new_post_form =  PostModelForm(request.POST)
         if new_post_form.is_valid():
             new_post_form.save()
             return redirect(reverse('home_page'))
 
-    return render(request, 'posts/create.html', {'new_post_form': new_post_form, 'post_tags_form': post_tags_form})
+    return render(request, 'posts/create.html', context)
 
 def view_post_page(request):
     return render(request, 'posts/view.html')
