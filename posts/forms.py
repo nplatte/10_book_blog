48ca01b09c6c9aa5137e4a5cdce2d3398e82145f
nplatte <77999574+nplatte@users.noreply.github.com>
2023-07-09 22:09:15 -0400

    Added new form and form tests

--- posts/forms.py
+++ posts/forms.py
@@ -1,15 +1,22 @@
 from django.forms import ModelForm
 from django import forms
-from posts.models import Post
+from posts.models import Post, Tag
 
 class PostModelForm(ModelForm):
     
     class Meta:
         model = Post
-        fields = ['title', 'book_author', 'book_title', 'post']
+        fields = ['title', 'book_author', 'book_title', 'post', 'tags']
+        tags = forms.Textarea()
         widgets = {
             'book_author': forms.TextInput(attrs={'id': 'author_entry'}),
             'title': forms.TextInput(attrs={'id': 'title_entry'}),
             'post': forms.Textarea(attrs={'id': 'post_entry'}),
-            'book_title': forms.TextInput(attrs={'id': 'book_title_entry'})
+            'book_title': forms.TextInput(attrs={'id': 'book_title_entry'}),
+            'tags': forms.Textarea(attrs={'id': 'tag_entry'})
         }
+
+
+class TagForm(forms.Form):
+
+    pass
--- posts/tests/test_forms.py
+++ posts/tests/test_forms.py
@@ -1,12 +1,17 @@
-from posts.forms import PostModelForm
+from posts.forms import PostModelForm, TagForm
 from django.test import TestCase
+from posts.models import Tag
 
 class TestPostModelForm(TestCase):
 
     def test_form_successful_submit(self):
+        t1, t2 = Tag(tag_name='t1'), Tag(tag_name='t2')
+        t1.save()
+        t2.save()
         new_form = PostModelForm({'title': 'A', 'book_author': 'V.E. Schwab',
                                   'book_title': 'A Darker Side of Magic',
-                                  'post': 'blahblahblah'})
+                                  'post': 'blahblahblah', 'tags': [t1, t2]})
+        self.assertEqual(new_form.errors, {})
         self.assertTrue(new_form.is_valid())
 
     def test_form_has_right_identifying_information(self):
@@ -16,5 +21,24 @@ class TestPostModelForm(TestCase):
         self.assertIn('id="title_entry"', form_as_html)
         self.assertIn('id="post_entry"', form_as_html)
         self.assertIn('id="book_title_entry"', form_as_html)
+        self.assertIn('id="tag_entry"', form_as_html)
 
-        
+    def test_form_validation_checks_for_hashtag(self):
+        new_form = PostModelForm({'title': 'A', 'book_author': 'V.E. Schwab',
+                                  'book_title': 'A Darker Side of Magic',
+                                  'post': 'blahblahblah', 'tags': 'test'})
+        self.assertEqual(len(new_form.errors['tags']), 1)
+        self.assertEqual(new_form.errors['tags'][0], 'Add a hashtag infront of the tag')
+
+
+class TestTagModelForm(TestCase):
+
+    def test_successful_form_submit(self):
+        pass
+
+    def test_form_has_right_identifying_info(self):
+        pass
+
+    def test_form_can_create_multiple_tags(self):
+        pass
+
